fbb04c42130b40bcad6538a73ba9d68a75d8284c
Secil <secilegen@gmail.com>
2023-03-26 15:18:27 +0200

    home page added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { useContext } from "react";                     
+import { AuthContext } from "../context/auth.context"; 
+ 
 
 function HomePage() {
+  const {isLoggedIn, user, logOutUser} = useContext(AuthContext)
   return (
-    <div>HomePage</div>
+    <div>
+
+      {isLoggedIn && (
+        <div></div>
+      )}
+      {!isLoggedIn && (
+        <div>
+        <h1>Happy plants, happy homes</h1>
+        <h5>We know how hard it can get to keep your plants alive. Planty is here to support you with caring tips and more to make your plants happy
+        </h5>
+        <Link to="/signup"> <button>Create an account</button> </Link>
+        <Link to="/login"> <button>Login</button> </Link>
+        <div>
+          <h1>Are you a plant expert?</h1>
+          <h5>Planty is a community where plant owners meet experts to get caring tips & tricks in person or online</h5>
+          <Link to="/signup-expert"> <button>Sign up as Planty expert</button> </Link>
+          </div>
+          </div>
+      )}
+      
+
+    </div>
   )
 }
 
