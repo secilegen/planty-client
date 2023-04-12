5ed57a9f129b6abaea3993ede56fbeb8dd018de4
Nadine Machmeier <info@nadinemachmeier.com>
2023-04-12 22:12:11 +0100

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import linkedIn from "../images/linkedIn.png"
 
 function Footer() {
   return (
@@ -10,6 +11,20 @@ function Footer() {
         <Link to="">
         <p>// Nadine Machmeier</p>
         </Link> */}
+
+<p>Planty created by</p>
+        <div className='footerText'>
+        <p className='footerLeft'> Secil Egen </p>
+        <Link to="https://www.linkedin.com/in/secilegen/">
+        <img src={linkedIn} alt="linkedin" className='LinkedInImg'/>
+        </Link>
+
+       <p className='footerRight'> & Nadine Machmeier</p>
+       <Link to="https://www.linkedin.com/in/nadine-machmeier/">
+        <img src={linkedIn} alt="linkedin" className='LinkedInImg'/>
+        </Link>
+        </div>
+
     </div>
   )
 }
