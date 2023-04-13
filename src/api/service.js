f40f391714e81f491b8b38e6b4e32e4ad76349b1
Secil <secilegen@gmail.com>
2023-04-13 21:40:04 +0300

    service file changed

--- src/api/service.js
+++ src/api/service.js
@@ -1,8 +1,10 @@
 import axios from "axios";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5005";
+
  
 const api = axios.create({
   // make sure you use PORT = 5005 (the port where our server is running)
-  baseURL: "http://localhost:5005/api"
+  baseURL: API_URL
   // withCredentials: true // => you might need this option if using cookies and sessions
 });
  
