7a1b9b5e090f497766a4a3977a5d9df6fe337b5a
Secil <secilegen@gmail.com>
2023-04-14 15:20:14 +0300

    loading spinner added

--- src/components/IsAnon.jsx
+++ src/components/IsAnon.jsx
@@ -1,12 +1,24 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
 import { Navigate } from "react-router-dom";
+import { TailSpin } from  'react-loader-spinner'
+
  
 function IsAnon( { children } ) {
   
   const { isLoggedIn, isLoading } = useContext(AuthContext);
 
-  if (isLoading) return <p>Loading ...</p>;
+  // if (isLoading) return <p>Loading ...</p>;
+   if (isLoading) return <TailSpin
+  height="80"
+  width="80"
+  color="#084d26"
+  ariaLabel="tail-spin-loading"
+  radius="1"
+  wrapperStyle={{}}
+  wrapperClass=""
+  visible={true}
+/>;
  
   if (isLoggedIn) {
     return <Navigate to="/" />;
--- src/components/IsPrivate.jsx
+++ src/components/IsPrivate.jsx
@@ -1,13 +1,23 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
 import { Navigate } from "react-router-dom";
+import { TailSpin} from "react-loader-spinner";
  
 function IsPrivate( { children } ) {
   
   const { isLoggedIn, isLoading } = useContext(AuthContext);
  
 
-  if (isLoading) return <p>Loading ...</p>
+  if (isLoading) return <TailSpin
+  height="80"
+  width="80"
+  color="#084d26"
+  ariaLabel="tail-spin-loading"
+  radius="1"
+  wrapperStyle={{}}
+  wrapperClass=""
+  visible={true}
+/>;
  
   if (!isLoggedIn) {
     return <Navigate to="/login" />;
